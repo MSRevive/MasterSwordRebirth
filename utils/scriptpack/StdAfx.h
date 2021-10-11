e413edcdd1d6d7413d476d8b962510e1edb334bb
Saint Wish <SaintWish@users.noreply.github.com>
2021-10-11 17:17:03 -0600

    add scriptpack source to the primary game repo

---
+++ utils/scriptpack/StdAfx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__0D3D71EE_53BD_40D1_8C4E_2EED9BB742B9__INCLUDED_)
+#define AFX_STDAFX_H__0D3D71EE_53BD_40D1_8C4E_2EED9BB742B9__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+// TODO: reference additional headers your program requires here
+#include "windows.h"
+#include "stdio.h"
+#include "packer.h"
+#include "../MSShared/groupfile.h"
+void Print( char *szFmt, ... );
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__0D3D71EE_53BD_40D1_8C4E_2EED9BB742B9__INCLUDED_)
