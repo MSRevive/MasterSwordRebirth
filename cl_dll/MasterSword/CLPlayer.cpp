5b883f517bbd299898aef4b0b72eaa8c0560dfb6
Saint Wish <SaintWish@users.noreply.github.com>
2021-12-02 14:39:57 -0700

    show item description

--- cl_dll/MasterSword/CLPlayer.cpp
+++ cl_dll/MasterSword/CLPlayer.cpp
@@ -954,6 +954,15 @@ int __MsgFunc_Item(const char *pszName, int iSize, void *pbuf)
 		pItem->CallScriptEvent(Event, &Params);
 	}
 	break;
+	
+	//Thothie OCT2016_23 - displaydesc
+	case 9:
+	{
+		ulong lID = READ_LONG ( );
+		CGenericItem *pItem = MSUtil_GetItemByID( lID );
+		ShowWeaponDesc( pItem );
+	}
+	break;
 	}
 
 	dbg("UpdateActiveMenus()");
@@ -1217,8 +1226,7 @@ int __MsgFunc_CLDllFunc(const char *pszName, int iSize, void *pbuf)
 		if (!iMode)
 		{
 			int flags = READ_BYTE();
-			bool fShowBrowser = flags & (1 << 0),
-				 fPlaySound = (flags & (1 << 1)) ? true : false;
+			bool fShowBrowser = flags & (1 << 0), fPlaySound = (flags & (1 << 1)) ? true : false;
 			 strncpy(player.m_NextMap,  READ_STRING(), sizeof(player.m_NextMap) );
 			 strncpy(player.m_OldTransition,  READ_STRING(), sizeof(player.m_OldTransition) );
 			 strncpy(player.m_NextTransition,  READ_STRING(), sizeof(player.m_NextTransition) );
