28ba421359336548f0b4543b4d06ae60c05d9a69
Saint Wish <SaintWish@users.noreply.github.com>
2022-10-07 23:00:35 -0600

    fix formatting

--- utils/scriptpack/packer.h
+++ utils/scriptpack/packer.h
@@ -10,35 +10,35 @@ typedef unsigned char byte;
 class Packer 
 {
 public:
-  Packer(char *wDir, char *rDir, char *oDir) {
-    _snprintf(m_WorkDir, MAX_PATH, "%s", wDir);
-    _snprintf(m_RootDir, MAX_PATH, "%s", rDir);
-    _snprintf(m_OutDir, MAX_PATH, "%s", oDir);
-    _snprintf(m_CookedDir, MAX_PATH, "%s\\cooked\\", rDir);
-    
-    try {
-      CreateDirectory(m_CookedDir, NULL);
-    }catch(...)
-    {
-      printf("Failed to create %s\n", m_CookedDir);
+	Packer(char *wDir, char *rDir, char *oDir) {
+		_snprintf(m_WorkDir, MAX_PATH, "%s", wDir);
+		_snprintf(m_RootDir, MAX_PATH, "%s", rDir);
+		_snprintf(m_OutDir, MAX_PATH, "%s", oDir);
+		_snprintf(m_CookedDir, MAX_PATH, "%s\\cooked\\", rDir);
+
+		try {
+			CreateDirectory(m_CookedDir, NULL);
+		}catch(...)
+		{
+			printf("Failed to create %s\n", m_CookedDir);
 			exit(-1);
-    }
-  }
+		}
+	}
   
-  void readDirectory(char *pszName, bool cooked);
-  void packScripts();
-  void cookScripts();
+	void readDirectory(char *pszName, bool cooked);
+	void packScripts();
+	void cookScripts();
   
 private:
-  char m_WorkDir[MAX_PATH];
-  char m_RootDir[MAX_PATH];
-  char m_CookedDir[MAX_PATH];
-  char m_OutDir[MAX_PATH];
-  msstringlist m_StoredFiles;
-  msstringlist m_CookedFiles;
-  
-  void storeFile(char *pszCurrentDir, WIN32_FIND_DATA &wfd, bool cooked);
-  void doParser(byte *buffer, size_t bufferSize, char *name, char *create, bool errOnly);
+	char m_WorkDir[MAX_PATH];
+	char m_RootDir[MAX_PATH];
+	char m_CookedDir[MAX_PATH];
+	char m_OutDir[MAX_PATH];
+	msstringlist m_StoredFiles;
+	msstringlist m_CookedFiles;
+
+	void storeFile(char *pszCurrentDir, WIN32_FIND_DATA &wfd, bool cooked);
+	void doParser(byte *buffer, size_t bufferSize, char *name, char *create, bool errOnly);
 };
 
 #endif
