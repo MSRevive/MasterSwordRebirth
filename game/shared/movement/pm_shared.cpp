9d5e5eba05ed3034ea34867b3a9f9daf892be0a1
Saint Wish <SaintWish@users.noreply.github.com>
2023-03-02 00:40:26 -0700

    fix linux compile

--- game/shared/movement/pm_shared.cpp
+++ game/shared/movement/pm_shared.cpp
@@ -330,7 +330,7 @@ void PM_InitTextureTypes()
 	std::size_t filePos = 0;
 
 	// for each line in the file...
-	while (memfgets(fileContents.data(), fileContents.size(), filePos, buffer, std::size(buffer) - 1) != nullptr && (gcTextures < CTEXTURESMAX))
+	while (memfgets(fileContents.data(), fileContents.size(), filePos, buffer, size(buffer) - 1) != nullptr && (gcTextures < CTEXTURESMAX))
 	{
 		// skip whitespace
 		i = 0;
