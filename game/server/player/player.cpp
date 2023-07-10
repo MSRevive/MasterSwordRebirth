7e9b4cdc88ee3c818c690c52e94a1e69eb509107
Saint Wish <SaintWish@users.noreply.github.com>
2023-07-10 05:28:25 -0600

    don't send empty track string

--- game/server/player/player.cpp
+++ game/server/player/player.cpp
@@ -6829,10 +6829,9 @@ bool CBasePlayer::SwapMusic(int musicArea, int mode, std::string track)
 		MESSAGE_BEGIN(MSG_ONE, g_netmsg[NETMSG_MUSIC], NULL, this->pev);
 		WRITE_BYTE(mode);
 
-		if (mode != MUSIC_STOP || track != "") //If playing music instead of stopping, send the music file
+		if (mode != MUSIC_STOP || track.length() > 0) //If playing music instead of stopping, send the music file
 		{
-			if (track.length() > 0) WRITE_STRING(track.c_str());
-			else WRITE_STRING("stop.mp3"); //have to send something. dont send empty strings
+			WRITE_STRING(track.c_str());
 		}
 
 		MESSAGE_END();
