a76b24f79dc6bd34631058d99a3d1213bb957fad
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2024-10-23 17:44:54 -0500

    allow cmake to use VCPKG for finding packages

--- cmake/LinuxToolchain.cmake
+++ cmake/LinuxToolchain.cmake
@@ -1,3 +1,7 @@
+# VCPKG
+set(VCPKG_TARGET_TRIPLET "x86-linux" CACHE INTERNAL "Triplet to use for Linux development. Do not modify.")
+include(${CMAKE_CURRENT_LIST_DIR}/../vcpkg/scripts/buildsystems/vcpkg.cmake)
+
 # the name of the target operating system
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR i386)
