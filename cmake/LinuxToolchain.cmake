ec667d2dc3d4b2faf77f6b992771efccee551ee4
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2024-10-24 01:56:46 -0500

    use static libraries

--- cmake/LinuxToolchain.cmake
+++ cmake/LinuxToolchain.cmake
@@ -1,4 +1,6 @@
 # VCPKG
+set(VCPKG_TARGET_ARCHITECTURE x86)
+set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_TARGET_TRIPLET "x86-linux" CACHE INTERNAL "Triplet to use for Linux development. Do not modify.")
 include(${CMAKE_CURRENT_LIST_DIR}/../vcpkg/scripts/buildsystems/vcpkg.cmake)
 
--- cmake/WindowsToolchain.cmake
+++ cmake/WindowsToolchain.cmake
@@ -1,3 +1,5 @@
 # VCPKG
+set(VCPKG_TARGET_ARCHITECTURE x86)
+set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_TARGET_TRIPLET "x86-windows" CACHE INTERNAL "Triplet to use for Windows development. Do not modify.")
 include(${CMAKE_CURRENT_LIST_DIR}/../vcpkg/scripts/buildsystems/vcpkg.cmake)
