5dca01d338066b4d521c4238555d146b87d68051
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2025-03-22 16:10:34 -0500

    change it back

--- cmake/LinuxToolchain.cmake
+++ cmake/LinuxToolchain.cmake
@@ -10,4 +10,4 @@ set(CMAKE_C_COMPILER gcc-11)
 set(CMAKE_CXX_COMPILER g++-11)
 set(CMAKE_CXX_FLAGS "-m32 -static-libgcc -static-libstdc++ -D_GLIBCXX_USE_CXX11_ABI=0")
 
-include(${CMAKE_CURRENT_LIST_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../vcpkg/scripts/buildsystems/vcpkg.cmake)
