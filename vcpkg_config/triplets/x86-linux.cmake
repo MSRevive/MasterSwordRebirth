192d805524a3a98acefbc69ed594c8faadd1a888
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2025-03-23 16:34:22 -0500

    load toolchain for linux triplet

--- vcpkg_config/triplets/x86-linux.cmake
+++ vcpkg_config/triplets/x86-linux.cmake
@@ -4,3 +4,5 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_BUILD_TYPE release)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE ${CMAKE_CURRENT_LIST_DIR}/../../cmake/LinuxToolchain.cmake)
