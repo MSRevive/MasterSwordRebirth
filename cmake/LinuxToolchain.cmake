5a4256bb702f245d029a55d1a48a0987ffe2a758
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2025-04-02 19:36:14 -0500

    Update LinuxToolchain.cmake

--- cmake/LinuxToolchain.cmake
+++ cmake/LinuxToolchain.cmake
@@ -1,13 +1,2 @@
 # VCPKG
 set(VCPKG_TARGET_TRIPLET "x86-linux" CACHE INTERNAL "Triplet to use for Linux development. Do not modify.")
-
-# the name of the target operating system
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR i386)
-
-# which compilers to use for C and C++
-set(CMAKE_C_COMPILER gcc-11)
-set(CMAKE_CXX_COMPILER g++-11)
-set(CMAKE_CXX_FLAGS "-m32 -static-libgcc -static-libstdc++ -D_GLIBCXX_USE_CXX11_ABI=0")
-
-include(${CMAKE_CURRENT_LIST_DIR}/../vcpkg/scripts/buildsystems/vcpkg.cmake)
