788474dd4ebab298a1dd8d5af0b22e7d1efb8027
tschumann <schumann.tom@gmail.com>
2025-04-12 13:49:01 +1000

    Update Linux build scripts.

--- build-linux.sh
+++ build-linux.sh
@@ -4,7 +4,20 @@ set -eu
 
 cd $(dirname "${BASH_SOURCE[0]}")
 
-./createall
+path=.
+type=Release
 
-make -C build
+while getopts "p:t:" opt; do
+	case "${opt}" in
+		p)
+			path=${OPTARG}
+			;;
+		t)
+			type=${OPTARG}
+			;;
+	esac
+done
 
+./createall.sh -p $path -t $type
+
+cmake --build $path/build --config $type
