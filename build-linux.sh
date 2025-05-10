aa43f9b15db132332db9ee44049b7109a5b1a9b9
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2025-05-09 19:21:04 -0500

    attempt to fix conflicts

--- build-linux.sh
+++ build-linux.sh
@@ -4,7 +4,7 @@ set -eu
 
 cd $(dirname "${BASH_SOURCE[0]}")
 
-path=.
+path=$(realpath .)
 type=Release
 
 while getopts "p:t:" opt; do
@@ -27,5 +27,3 @@ gcc-11 -v
 ./createall.sh -p $path -t $type
 
 make -C $path/build
-
-
