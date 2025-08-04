7ee0fc7c50e54702219f59dde14c8e0ae2c1239d
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2025-08-04 17:42:23 -0500

    add macro for render logging

--- src/game/shared/ms/mslogger.h
+++ src/game/shared/ms/mslogger.h
@@ -121,6 +121,9 @@ private:
 #define MS_ANGEL_INFO(fmt, ...) MSLogger::Info(MSLogger::ANGELSCRIPT, fmt, ##__VA_ARGS__)
 #define MS_ANGEL_ERROR(fmt, ...) MSLogger::Error(MSLogger::ANGELSCRIPT, fmt, ##__VA_ARGS__)
 #define MS_ANGEL_DEBUG(fmt, ...) MSLogger::Debug(MSLogger::ANGELSCRIPT, fmt, ##__VA_ARGS__)
+#define MS_RENDER_INFO(fmt, ...) MSLogger::Info(MSLogger::RENDER, fmt, ##__VA_ARGS__)
+#define MS_RENDER_WARN(fmt, ...) MSLogger::Warn(MSLogger::RENDER, fmt, ##__VA_ARGS__)
+#define MS_RENDER_ERROR(fmt, ...) MSLogger::Error(MSLogger::RENDER, fmt, ##__VA_ARGS__)
 
 // Compatibility macro for gradual ALERT migration
 // Note: Engine headers should not be included here to avoid circular dependencies
