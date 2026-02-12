57c26f8b1f09e840a4fb4bc970441a890de527ed
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2026-02-12 17:34:02 -0600

    debugging

--- src/game/shared/weapons/giattack.cpp
+++ src/game/shared/weapons/giattack.cpp
@@ -264,6 +264,7 @@ bool CGenericItem::StartAttack(int ForceAttackNum)
 		CurrentAttack = &m_Attacks[ForceAttackNum];
 		iNewAttack = ForceAttackNum;
 	}
+	
 #ifndef VALVE_DLL
 	else if (!CurrentAttack)
 	{
@@ -781,7 +782,8 @@ void CGenericItem::StrikeLand()
 
 	//	flDmgFraction = max(mSStat( CurrentAttack->sAttackStat, STATPROP_POWER ) / STATPROP_MAX_VALUE,0);
 	//	ALERT( at_console, "Use stat: %i  Value: %i %i %i\n", GetStatByName( (char*)STRING(CurrentAttack->sAttackStat) ), mSStat( CurrentAttack->sAttackStat, STATPROP_SPEED ), mSStat( CurrentAttack->sAttackStat, STATPROP_BALANCE ), mSStat( CurrentAttack->sAttackStat, STATPROP_POWER ) );
-	float flDamage = CurrentAttack->flDamage + RANDOM_LONG(-CurrentAttack->flDamageRange, CurrentAttack->flDamageRange);
+	long randomMulti = RANDOM_LONG(0, CurrentAttack->flDamageRange);
+	float flDamage = CurrentAttack->flDamage + randomMulti;
 	flDamage *= flDmgFraction;
 	if (bUnderleveled)
 		flDamage *= 0.5; //this might be working
@@ -1880,11 +1882,11 @@ CBaseEntity *DoDamage(damage_t &Damage, CBaseEntity *pTarget)
 				else if (dtype_code.starts_with("earth"))
 					 strncpy(element_code,  " earth", sizeof(element_code) );
 
-				 strncpy(szDamage, Damage.AttackHit ? UTIL_VarArgs("%.1f%s damage.", Damage.flDamage, element_code) : "", sizeof(szDamage) );
-				 strncpy(szHitMiss, Damage.AttackHit ? "HIT!" : (fDodged ? "PARRIED!" : "MISS!"), sizeof(szHitMiss));
-				 //our hits can not be inverted or they will seem weird with no accuracy
-				 // this is no longer needed with missing being only possible during lightning debuffs. Enabling this would mean missing with high numbers during lightning so obfuscating.
-				 //_snprintf(szStats, sizeof(szStats), "(%i)",  (iAccuracyRoll) );
+				strncpy(szDamage, Damage.AttackHit ? UTIL_VarArgs("%.1f%s damage.", Damage.flDamage, element_code) : "", sizeof(szDamage) );
+				strncpy(szHitMiss, Damage.AttackHit ? "HIT!" : (fDodged ? "PARRIED!" : "MISS!"), sizeof(szHitMiss));
+				//our hits can not be inverted or they will seem weird with no accuracy
+				// this is no longer needed with missing being only possible during lightning debuffs. Enabling this would mean missing with high numbers during lightning so obfuscating.
+				//_snprintf(szStats, sizeof(szStats), "(%i)",  (iAccuracyRoll) );
 				
 				//Thothie SEP2019_22 - report resistance BEGIN
 				bool tdm_found_entry = false;
