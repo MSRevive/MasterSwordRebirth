5f0c440ede40bf09a50b3f5c36977a0b8668243d
Rederick5 <73010056+Rederick5@users.noreply.github.com>
2026-02-23 18:37:57 -0500

    Removed m_LastChargedAmt modifiers in StrikeLand() Charge damage is calculated through scripting.

--- src/game/shared/weapons/giattack.cpp
+++ src/game/shared/weapons/giattack.cpp
@@ -788,13 +788,6 @@ void CGenericItem::StrikeLand()
 	if (bUnderleveled)
 		flDamage *= 0.5; //this might be working
 
-	//Print( "In damage: %f\nLast charged: %f\n", flDamage, m_LastChargedAmt );
-	if (m_LastChargedAmt < 1)
-	{
-		flDamage *= (m_LastChargedAmt + 1);
-		//Print("Out damage: %f\nCharge Multiplier: %f\n", flDamage, (m_LastChargedAmt+1));
-	}
-
 	SetDebugProgress(ItemThinkProgress, "CGenericItem::StrikeLand - Call DoDamage");
 	int bitsDamage = DMG_CLUB;
 	if (!m_pPlayer)
