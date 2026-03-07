b8c0fda959cd28debc19474fc52b7c76e8942fbb
Rederick5 <73010056+Rederick5@users.noreply.github.com>
2026-03-07 03:25:00 -0500

    Changed more macros to constexpr int

--- src/common/dll_state.h
+++ src/common/dll_state.h
@@ -7,17 +7,17 @@
 
 //DLL State Flags
 
-#define DLL_INACTIVE 0 // no dll
-#define DLL_ACTIVE 1   // dll is running
-#define DLL_PAUSED 2   // dll is paused
-#define DLL_CLOSE 3    // closing down dll
-#define DLL_TRANS 4    // Level Transition
+constexpr int DLL_INACTIVE = 0; // no dll
+constexpr int DLL_ACTIVE = 1;   // dll is running
+constexpr int DLL_PAUSED = 2;   // dll is paused
+constexpr int DLL_CLOSE = 3;    // closing down dll
+constexpr int DLL_TRANS = 4;    // Level Transition
 
 // DLL Pause reasons
 
-#define DLL_NORMAL 0  // User hit Esc or something.
-#define DLL_QUIT 4    // Quit now
-#define DLL_RESTART 6 // Switch to launcher for linux, does a quit but returns 1
+constexpr int DLL_NORMAL = 0;  // User hit Esc or something.
+constexpr int DLL_QUIT = 4;    // Quit now
+constexpr int DLL_RESTART = 6; // Switch to launcher for linux, does a quit but returns 1
 
 // DLL Substate info ( not relevant )
-#define ENG_NORMAL (1 << 0)
+constexpr int ENG_NORMAL = (1 << 0);
