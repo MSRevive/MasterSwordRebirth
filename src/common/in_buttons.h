fadbe4edb87d30c846ef2bb2dcd9ef265e60788d
Rederick5 <73010056+Rederick5@users.noreply.github.com>
2026-03-07 18:36:25 -0500

    Changed macros to constexpr

--- src/common/in_buttons.h
+++ src/common/in_buttons.h
@@ -18,21 +18,21 @@
 #pragma once
 #endif
 
-#define IN_ATTACK (1 << 0)
-#define IN_JUMP (1 << 1)
-#define IN_DUCK (1 << 2)
-#define IN_FORWARD (1 << 3)
-#define IN_BACK (1 << 4)
-#define IN_USE (1 << 5)
-#define IN_CANCEL (1 << 6)
-#define IN_LEFT (1 << 7)
-#define IN_RIGHT (1 << 8)
-#define IN_MOVELEFT (1 << 9)
-#define IN_MOVERIGHT (1 << 10)
-#define IN_ATTACK2 (1 << 11)
-#define IN_RUN (1 << 12)
-#define IN_RELOAD (1 << 13)
-#define IN_ALT1 (1 << 14)
-#define IN_SCORE (1 << 15) // Used by client.dll for when scoreboard is held down
+constexpr int IN_ATTACK = (1 << 0);
+constexpr int IN_JUMP = (1 << 1);
+constexpr int IN_DUCK = (1 << 2);
+constexpr int IN_FORWARD = (1 << 3);
+constexpr int IN_BACK = (1 << 4);
+constexpr int IN_USE = (1 << 5);
+constexpr int IN_CANCEL = (1 << 6);
+constexpr int IN_LEFT = (1 << 7);
+constexpr int IN_RIGHT = (1 << 8);
+constexpr int IN_MOVELEFT = (1 << 9);
+constexpr int IN_MOVERIGHT = (1 << 10);
+constexpr int IN_ATTACK2 = (1 << 11);
+constexpr int IN_RUN = (1 << 12);
+constexpr int IN_RELOAD = (1 << 13);
+constexpr int IN_ALT1 = (1 << 14);
+constexpr int IN_SCORE = (1 << 15);// Used by client.dll for when scoreboard is held down
 
 #endif // IN_BUTTONS_H
