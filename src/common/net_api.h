e93b7e4e88ce9e90ee5c23f4ee4c4c4f034eabce
Rederick5 <73010056+Rederick5@users.noreply.github.com>
2026-03-08 17:57:18 -0400

    changed constexpr to enum

--- src/common/net_api.h
+++ src/common/net_api.h
@@ -15,22 +15,29 @@
 #include "netadr.h"
 #endif
 
-constexpr int NETAPI_REQUEST_SERVERLIST = 0; // Doesn't need a remote address
-constexpr int NETAPI_REQUEST_PING = 1;
-constexpr int NETAPI_REQUEST_RULES = 2;
-constexpr int NETAPI_REQUEST_PLAYERS = 3;
-constexpr int NETAPI_REQUEST_DETAILS = 4;
+
+enum {
+	NETAPI_REQUEST_SERVERLIST = 0, // Doesn't need a remote address
+	NETAPI_REQUEST_PING = 1,
+	NETAPI_REQUEST_RULES = 2,
+	NETAPI_REQUEST_PLAYERS = 3,
+	NETAPI_REQUEST_DETAILS = 4
+};
 
 // Set this flag for things like broadcast requests, etc. where the engine should not
 //  kill the request hook after receiving the first response
-constexpr int FNETAPI_MULTIPLE_RESPONSE = (1 << 0);
+enum {
+	FNETAPI_MULTIPLE_RESPONSE = (1 << 0);
+};
 
 typedef void (*net_api_response_func_t)(struct net_response_s *response);
 
-constexpr int NET_SUCCESS = 0;
-constexpr int NET_ERROR_TIMEOUT = 1 << 0;
-constexpr int NET_ERROR_PROTO_UNSUPPORTED = 1 << 1;
-constexpr int NET_ERROR_UNDEFINED = 1 << 2;
+enum {
+	NET_SUCCESS = 0,
+	NET_ERROR_TIMEOUT = 1 << 0,
+	NET_ERROR_PROTO_UNSUPPORTED = 1 << 1,
+	NET_ERROR_UNDEFINED = 1 << 2
+};
 
 typedef struct net_adrlist_s
 {
