43cf79d9e94e02a0bacf2e15a6057fe5879960af
Rederick5 <73010056+Rederick5@users.noreply.github.com>
2026-03-21 22:34:59 -0400

    changed defs to enum

--- src/game/server/hl/trains.h
+++ src/game/server/hl/trains.h
@@ -16,24 +16,31 @@
 #define TRAINS_H
 
 // Tracktrain spawn flags
-#define SF_TRACKTRAIN_NOPITCH 0x0001
-#define SF_TRACKTRAIN_NOCONTROL 0x0002
-#define SF_TRACKTRAIN_FORWARDONLY 0x0004
-#define SF_TRACKTRAIN_PASSABLE 0x0008
-#define SF_TRACKTRAIN_NOYAW 0x0010 //APR2008a - No Yaw option for Tracktrain
+enum sf_tracktrain_e {
+	SF_TRACKTRAIN_NOPITCH = 0x0001,
+	SF_TRACKTRAIN_NOCONTROL = 0x0002,
+	SF_TRACKTRAIN_FORWARDONLY = 0x0004,
+	SF_TRACKTRAIN_PASSABLE = 0x0008,
+	SF_TRACKTRAIN_NOYAW = 0x0010, //APR2008a - No Yaw option for Tracktrain
+};
 
 // Spawnflag for CPathTrack
-#define SF_PATH_DISABLED 0x00000001
-#define SF_PATH_FIREONCE 0x00000002
-#define SF_PATH_ALTREVERSE 0x00000004
-#define SF_PATH_DISABLE_TRAIN 0x00000008
-#define SF_PATH_ALTERNATE 0x00008000
+
+enum sf_cpathtrack_e {
+	SF_PATH_DISABLED = 0x00000001,
+	SF_PATH_FIREONCE = 0x00000002,
+	SF_PATH_ALTREVERSE = 0x00000004,
+	SF_PATH_DISABLE_TRAIN = 0x00000008,
+	SF_PATH_ALTERNATE = 0x00008000
+};
 
 // Spawnflags of CPathCorner
-#define SF_CORNER_WAITFORTRIG 0x001
-#define SF_CORNER_TELEPORT 0x002
-#define SF_CORNER_FIREONCE 0x004
 
+enum sf_cpath_corner_e {
+	SF_CORNER_WAITFORTRIG = 0x001,
+	SF_CORNER_TELEPORT = 0x002,
+	SF_CORNER_FIREONCE = 0x004
+};
 //#define PATH_SPARKLE_DEBUG		1	// This makes a particle effect around path_track entities for debugging
 class CPathTrack : public CPointEntity
 {
